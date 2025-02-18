e7897881e6bf03b2664be5700b05176f9661afb5
icerank <icedann>
2025-02-18 16:44:27 -0500

    first commit

---
+++ plugins/gc-tagall.js
@@ -0,0 +1,19 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+  if (!(isAdmin || isOwner)) {
+  global.dfail('admin', m, conn)
+  throw false
+  }
+  let pesan = args.join` `
+  let oi = `*𝐌𝐞𝐧𝐬𝐚𝐣𝐞:* ${pesan}`
+  let teks =`*ETIQUETAL GENERAL* 👉🏻https://www.instagram.com/melsitaaaay\n\n🌩️ ${oi}\n\n🌩️ *𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚:*\n`
+  for (let mem of participants) {
+  teks += `🌩️ @${mem.id.split('@')[0]}\n`}
+  teks += `𝐁𝐨𝐭 𝐌𝐞𝐥𝐬𝐢𝐭𝐚`
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+  }
+  handler.help = ['tagall <mesaje>','invocar <mesaje>']
+  handler.tags = ['group']
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+  handler.admin = true
+  handler.group = true
+  export default handler
